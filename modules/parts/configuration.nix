ce6c98be7013a247ac30d1dfbf2ff51fec3be1d7
Denis Bekasow <denis.bekasow@dmi.de>
2026-04-20 12:08:16 +0200

    feat: add iso flag to control per-host iso build

--- modules/parts/configuration.nix
+++ modules/parts/configuration.nix
@@ -9,6 +9,7 @@ let
         system = lib.mkOption { type = str; default = "x86_64-linux"; };
         modules = lib.mkOption { type = listOf deferredModule; default = [ ]; };
         members = lib.mkOption { type = listOf str; default = [ ]; };
+        iso = lib.mkOption { type = bool; default = true; };
       };
     }];
   };
@@ -44,7 +45,7 @@ in
 
     perSystem = { system, ... }: {
       packages = lib.pipe config.dotnix [
-        (lib.filterAttrs (lib.const (host: host.system == system)))
+        (lib.filterAttrs (lib.const (host: host.iso && host.system == system)))
         (lib.concatMapAttrs (hostname: lib.const {
           "${hostname}-iso" = config.flake.nixosConfigurations.${hostname}.config.system.build.images.iso-installer;
         }))
