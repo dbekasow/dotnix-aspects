96c44c0f150761f0c9433a5ad96d19c37ee2c496
Denis Bekasow <denis.bekasow@dmi.de>
2026-04-24 07:47:12 +0200

    feat: configure dms and niri

--- modules/aspects/desktop/compositor/niri.nix
+++ modules/aspects/desktop/compositor/niri.nix
@@ -1,35 +1,33 @@
 { inputs, ... }: {
-  flake.modules.nixos.niri = {
+  flake.modules.nixos.niri = { pkgs, ... }: {
     imports = [ inputs.niri.nixosModules.niri ];
-
-    programs.niri.enable = true;
 
     nix.settings = {
       substituters = [ "https://niri.cachix.org" ];
       trusted-public-keys = [ "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964=" ];
     };
+
+    nixpkgs.overlays = [ inputs.niri.overlays.niri ];
+
+    programs.niri.enable = true;
+    programs.niri.package = pkgs.niri-unstable;
+
+    # Disable broken polkit-kde-agent, use polkit-gnome instead
+    systemd.user.services.niri-flake-polkit.enable = false;
   };
 
   flake.modules.homeManager.niri = {
     programs.niri.settings = {
-      spawn-at-startup = [ ];
-
       input = {
+        keyboard.xkb.options = "caps:escape";
         keyboard.xkb.layout = "de";
         touchpad = {
           accel-profile = "adaptive";
+          dwt = true;
           tap = true;
           natural-scroll = true;
-          dwt = true;
           middle-emulation = true;
         };
-      };
-
-      layout = {
-        background-color = "transparent";
-        border.width = 2;
-        focus-ring.width = 2;
-        gaps = 6;
       };
     };
   };
