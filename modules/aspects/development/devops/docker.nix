de373fbdb1a7d81b6fe37878f718d1f9b39ff8c0
Denis Bekasow <denis.bekasow@gmail.com>
2026-04-27 20:23:37 +0200

    refactor: remove docker storageDriver

--- modules/aspects/development/devops/docker.nix
+++ modules/aspects/development/devops/docker.nix
@@ -2,24 +2,18 @@
   flake.modules.nixos.docker = { config, lib, ... }: {
     virtualisation.docker = {
       enable = true;
-      enableNvidia = false;
 
       rootless.enable = true;
       rootless.setSocketVariable = true;
 
-      storageDriver = "overlay2";
-
-      autoPrune = {
-        enable = true;
-        dates = "weekly";
-        flags = [ "--all" ];
-      };
+      autoPrune.enable = true;
     };
 
     users.users = lib.genAttrs config.dotnix.host.members (lib.const {
       # required for rootless docker user namespace mapping
       subUidRanges = [{ startUid = 100000; count = 65536; }];
       subGidRanges = [{ startGid = 100000; count = 65536; }];
+      linger = true;
     });
   };
 
