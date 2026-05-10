dcec3c7fbd01f04217cc92aaafad6ff7e76bf6cf
Denis Bekasow <denis.bekasow@gmail.com>
2026-05-10 14:21:46 +0200

    feat: use lowercase names for xdg userdirs

--- modules/aspects/desktop.nix
+++ modules/aspects/desktop.nix
@@ -21,6 +21,7 @@
       niri
       onlyoffice
       thunderbird
+      xdg
     ];
   };
 }
--- modules/aspects/system.nix
+++ modules/aspects/system.nix
@@ -1,15 +1,20 @@
 { self, ... }: {
-  flake.modules = let inherit (self.modules) nixos; in {
+  flake.modules = let inherit (self.modules) nixos homeManager; in {
     nixos.system.imports = with nixos; [
       bluetooth
       boot
       boot-systemd
       disko
+      impermanence
       geolocation
       network
       network-wifi
       pipewire
       power
+    ];
+
+    homeManager.system.imports = with homeManager; [
+      impermanence
     ];
   };
 }
