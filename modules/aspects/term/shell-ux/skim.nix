b6381990856360aa4473c3a9dcbe0642aa3c6d92
Denis Bekasow <denis.bekasow@dmi.de>
2026-05-13 11:54:32 +0200

    feat: configure fzf and skim

--- modules/aspects/term/shell-ux/skim.nix
+++ modules/aspects/term/shell-ux/skim.nix
@@ -2,6 +2,23 @@
   flake.modules.homeManager.skim = {
     programs.skim = {
       enable = true;
+
+      defaultCommand = "fd --type f";
+      defaultOptions = [ "--height 50%" "--layout reverse" ];
+
+      changeDirWidgetCommand = "fd --type d";
+      changeDirWidgetOptions = [
+        "--preview 'eza --tree --icons --color=always --level 3 --git-ignore {}'"
+        "--pointer ' '"
+      ];
+
+      fileWidgetCommand = "fd --type f";
+      fileWidgetOptions = [
+        "--preview 'bat --color=always {}'"
+        "--pointer ' '"
+      ];
+
+      historyWidgetOptions = [ "--pointer ' '" ];
     };
   };
 }
