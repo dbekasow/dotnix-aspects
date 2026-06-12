2cb2e3f3a331557aa168144eb720a26722fb9672
Denis Bekasow <denis.bekasow@dmi.de>
2026-06-12 16:38:29 +0200

    fix: test connection bevor cloning git repo

--- modules/aspects/development/vcs/git-repos.nix
+++ modules/aspects/development/vcs/git-repos.nix
@@ -1,18 +1,32 @@
 {
-  flake.modules.homeManager.git-repos = { config, lib, pkgs, ... }: {
-    options.dotnix.git.repositories = lib.mkOption {
-      type = with lib.types; attrsOf str;
-      default = { };
-    };
+  flake.modules.homeManager.git-repos = { config, lib, pkgs, ... }:
+    let
+      cloneRepo = pkgs.writeShellApplication {
+        name = "clone-repo";
+        runtimeInputs = [ pkgs.git ];
+        text = ''
+          dest="$HOME/$1"
+          url="$2"
 
-    config.home.activation.cloneGitRepos = lib.hm.dag.entryAfter [ "writeBoundary" ] (
-      lib.concatStrings (lib.mapAttrsToList
-        (dest: url: ''
-          if [ ! -d "$HOME/${dest}/.git" ]; then
-            $DRY_RUN_CMD ${lib.getExe pkgs.git} clone "${url}" "$HOME/${dest}"
-          fi
-        '')
-        config.dotnix.git.repositories)
-    );
-  };
+          [ -d "$dest/.git" ] && exit 0
+          git ls-remote "$url" HEAD >/dev/null 2>&1 || {
+            echo "skip: $url unreachable" >&2
+            exit 0
+          }
+          git clone "$url" "$dest"
+        '';
+      };
+    in
+    {
+      options.dotnix.git.repositories = lib.mkOption {
+        type = with lib.types; attrsOf str;
+        default = { };
+      };
+
+      config.home.activation.cloneGitRepos = lib.hm.dag.entryAfter [ "writeBoundary" ] (
+        lib.concatLines (lib.mapAttrsToList
+          (dest: url: "$DRY_RUN_CMD ${lib.getExe cloneRepo} ${dest} ${url}")
+          config.dotnix.git.repositories)
+      );
+    };
 }
