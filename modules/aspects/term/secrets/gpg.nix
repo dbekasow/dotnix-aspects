d3365ff062bf4fc43f0f626a106103fe7ea978df
Denis Bekasow <denis.bekasow@gmail.com>
2026-06-14 10:58:50 +0200

    feat: increse cache time for gpg

--- modules/aspects/term/secrets/gpg.nix
+++ modules/aspects/term/secrets/gpg.nix
@@ -8,10 +8,16 @@
   flake.modules.homeManager.gpg-agent = { pkgs, ... }: {
     programs.gpg.enable = true;
     programs.gpg.scdaemonSettings.disable-ccid = true;
-    services.gpg-agent = {
+    services.gpg-agent = rec {
       enable = true;
       enableSshSupport = true;
       pinentry.package = pkgs.pinentry-gnome3;
+
+      defaultCacheTtl = 7200; # 2h idle cache for normal keys
+      maxCacheTtl = 28800; # 8h absolute cap
+
+      defaultCacheTtlSsh = defaultCacheTtl;
+      maxCacheTtlSsh = maxCacheTtl;
     };
   };
 
